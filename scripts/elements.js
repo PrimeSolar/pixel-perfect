be3203328a541b8f4f6067b06c0c7104f8df4fd4
I am <88476416+FirstSolar@users.noreply.github.com>
2024-06-29 11:56:05 +0300

    Create elements.js
    Create script for reusable web elements

---
+++ scripts/elements.js
@@ -0,0 +1,16 @@
+// Reusable Elements
+
+// Footer
+footerContainer = document.querySelector("#footerContainer");
+
+function footerContainerF(footerContainer) {
+  if (footerContainer != null) {
+    // Define footer content:
+    footerContainer.innerHTML += `
+<footer>
+Copyright © <a href="https://firstsolar.github.io/web-developer/">Vladislav Kazantsev</a> 2024
+</footer>
+      `
+  }
+}
+footerContainerF(footerContainer);
