c55e4c0015ffe95c304ffa42496ba9d994ac9a69
I am <88476416+PrimeSolar@users.noreply.github.com>
2025-05-30 21:46:17 +0300

    Create meta-tags.js

---
+++ scripts/meta-tags.js
@@ -0,0 +1,64 @@
+// Set the <html> element `lang` property
+const html = document.querySelector("html");
+html.lang = "en";
+
+// Set `dir="ltr"` for the <html> element
+html.dir = "ltr";
+
+// Set <meta name="viewport" content="width=device-width, initial-scale=1" />
+const metaViewport = document.createElement("meta");
+metaViewport.name = "viewport";
+metaViewport.content = "width=device-width, initial-scale=1";
+document.querySelector("head").appendChild(metaViewport);
+
+// Set `title` for the <html> element based on a webpage's content
+const title = document.querySelector("h1");
+document.title = title.textContent;
+
+// Insert <meta name="author" content="Vladislav Kazantsev">
+const metaAuthor = document.createElement("meta");
+metaAuthor.name = "author";
+metaAuthor.content = "Vladislav Kazantsev";
+document.querySelector("head").appendChild(metaAuthor);
+
+// Insert <meta name="copyright" content="© `Current Year` Vladislav Kazantsev">
+const metaCopyright = document.createElement("meta");
+metaCopyright.name = "copyright";
+metaCopyright.content =
+  "© " + new Date().getFullYear() + " Vladislav Kazantsev";
+document.querySelector("head").appendChild(metaCopyright);
+
+// Insert <link rel="stylesheet" type="text/css" href="styles/style-min.css">
+const linkStyle = document.createElement("link");
+linkStyle.rel = "stylesheet";
+linkStyle.type = "text/css";
+linkStyle.href = "styles/style-min.css";
+document.querySelector("head").appendChild(linkStyle);
+linkStyle.onload = function () {
+  // Show the body after the stylesheet is loaded
+  document.body.style.display = "block";
+};
+
+// Insert <link rel="stylesheet" type="text/css" href="styles/media-queries-min.css">
+const linkMediaQueries = document.createElement("link");
+linkMediaQueries.rel = "stylesheet";
+linkMediaQueries.type = "text/css";
+linkMediaQueries.href = "styles/media-queries-min.css";
+document.querySelector("head").appendChild(linkMediaQueries);
+
+// Insert <link rel="icon" href="assets/favicon.jpg">
+const linkImage = document.createElement("link");
+linkImage.rel = "icon";
+linkImage.href = "assets/favicon.jpg";
+document.querySelector("head").appendChild(linkImage);
+
+// Set `type="text/javascript"` for all <script> elements for better compatibility
+for (let x of document.querySelectorAll("script")) {
+  x.type = "text/javascript";
+}
+
+// Insert <meta http-equiv="Pragma" content="no-cache">
+// var metaPragma = document.createElement("meta");
+// metaPragma.httpEquiv = "Pragma";
+// metaPragma.content = "no-cache";
+// document.querySelector("head").appendChild(metaPragma);
