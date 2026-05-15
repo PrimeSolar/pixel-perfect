3eca7dc986036316e9517415424e14701fc37ea8
I am <88476416+PrimeSolar@users.noreply.github.com>
2026-05-15 23:59:06 +0300

    Update scroll-animation.js

--- scripts/scroll-animation.js
+++ scripts/scroll-animation.js
@@ -7,7 +7,7 @@
  * You are welcome to clone the related repository and use the code for exploratory purposes.
  * However, unauthorized reproduction, modification, or redistribution of this code (including cloning of related repository or altering it for activities beyond exploratory use) is strictly prohibited.
  * Code snippets may be shared only when the original author is explicitly credited and a direct link to the original source of the code is provided alongside the code snippet.
- * Sharing the link to the file is 
+ * Sharing the link to the file is
  * permitted, except when directed toward retrieval purposes.
  * Any form of interaction with this file is strictly prohibited when facilitated by the code, except when such interaction is for discussion or exchange purposes with others.
  * This copyright notice applies globally.
@@ -16,6 +16,10 @@
 
 const sections = document.querySelectorAll(".scroll-animation");
 
+/**
+ * The script includes detailed comments
+ * to support stakeholders with varying JS knowledge.
+ */
 const options = {
   root: null,
   threshold: 0.1,
